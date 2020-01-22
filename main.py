d406787c991fb0caab405179bea5639fd164ee51
Moriarty002 <43646155+Moriarty002@users.noreply.github.com>
2020-01-22 19:20:52 +0800

    Ui fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 if __name__ == "__main__":
    app = QtWidgets.QApplication(sys.argv)
    MainWindow = QtWidgets.QMainWindow()
-   ui = Ui_MainWindow()
+   ui = Ui_ALC.Ui_AzurLaneCheater()
    ui.setupUi(MainWindow)
    MainWindow.show()
    sys.exit(app.exec_())
