70f5e6ebad17712bccfbd0764cb21103e8b588db
Moriarty002 <43646155+Moriarty002@users.noreply.github.com>
2020-01-28 21:33:17 +0800

    map_alg + mouse pos detect

--- tools.py
+++ tools.py
@@ -47,3 +47,47 @@ def mprtsc(x1=0,y1=0,x2=100,y2=100):
     img = ImageGrab.grab(bbox=(x1,y1,x2,y2))
     img.save('test.jpg','JPEG')
 
+'''
+map generate
+'''
+
+'''
+map caculate
+map_cac(map,tpos,epos) >> args: map(ndarray),tpos==team1 position(tupple),epos==enemy position(tupple array)
+'''
+def bfs(omap,nmap,vmap,start):
+    x=start[0]
+    y=start[1]
+    if x<0 or x>=omap.shape[0]:
+        return nmap
+    elif y<0 or y>=omap.shape[1]:
+        return nmap
+    if vmap[x,y] == 1:
+        return nmap
+    vmap[x,y]=1
+    nmap[x,y]=0
+    if omap[x,y] == 0 :
+        nmap[x,y]=1
+        nmap=bfs(omap,nmap,vmap,(x+1,y))
+        nmap=bfs(omap,nmap,vmap,(x-1,y))
+        nmap=bfs(omap,nmap,vmap,(x,y+1))
+        nmap=bfs(omap,nmap,vmap,(x,y-1))
+    elif omap[x,y] == 1:
+        nmap[x,y]=1
+    return nmap
+def add_enymy(mmap,pos):
+    mmap[pos[0],pos[1]]=1
+    return mmap
+def map_cac(map,tpos,epos):
+    #map_3_4 0=通道 -1=路障 1=敵人
+    #abmap 0=無法到達 1=可到達
+    #tmap 2=點擊目標 其他=非必要點擊
+    abmap=np.zeros_like(map)
+    vmap=abmap
+    for i in epos:
+        map=add_enymy(map,i)
+    abmap=bfs(map,abmap,vmap,tpos)
+    tmap=abmap+map
+    return tmap ,map
+
+
